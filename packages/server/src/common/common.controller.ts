8a7d49e43ef89cccb79885726973089ee17335fe
w-f-g <1035550175@qq.com>
2023-12-19 23:01:00 +0800

    update: 文件上传功能完善

--- packages/server/src/common/common.controller.ts
+++ packages/server/src/common/common.controller.ts
@@ -1,18 +1,49 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import { Controller, Post, UploadedFile, UseInterceptors, Req, Inject } from '@nestjs/common'
 import { CommonService } from './common.service'
 import R from 'src/utils/response'
-import { SkipAuth } from 'src/auth/skip-auth.decorator'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { diskStorage } from 'multer'
 import { extname, resolve } from 'path'
 import { v4 as uuid_v4 } from 'uuid'
+import { Request } from 'express'
+import os from 'os'
+
+function getNetworkIp() {
+  let needHost = '' // 打开的host
+  try {
+    // 获得网络接口列表
+    const network = os.networkInterfaces()
+    // console.log("network",network)
+    for (const dev in network) {
+      const iface = network[dev]
+      for (let i = 0; i < iface.length; i++) {
+        const alias = iface[i]
+        if (
+          alias.family === 'IPv4' &&
+          alias.address !== '127.0.0.1' &&
+          !alias.internal
+        ) {
+          needHost = alias.address
+        }
+      }
+    }
+  } catch (e) {
+    needHost = 'localhost'
+  }
+  return needHost
+}
 
 @Controller('/admin/common')
 export class CommonController {
+  @Inject(CommonService)
+  private readonly commonService: CommonService
 
-  constructor(private readonly commonService: CommonService) {}
+  private host: string
 
-  @SkipAuth()
+  constructor() {
+    this.host = getNetworkIp()
+  }
+
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file', {
     storage: diskStorage({
@@ -31,7 +62,9 @@ export class CommonController {
       }
     })
   }))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return R.success(`/public/images/${file.filename}`)
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
+    const host = this.host
+    const protocol = req.protocol
+    return R.success(`${protocol}://${host}:${process.env.PORT}/public/images/${file.filename}`)
   }
 }
