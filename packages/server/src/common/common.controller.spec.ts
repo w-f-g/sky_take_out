d830a40363c3d42d17d76706bc4d51a21a51f2bf
w-f-g <1035550175@qq.com>
2024-02-08 23:24:12 +0800

    fix: 服务端图片上传接口完善

--- packages/server/src/common/common.controller.spec.ts
+++ packages/server/src/common/common.controller.spec.ts
@@ -1,20 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CommonController } from './common.controller';
-import { CommonService } from './common.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { CommonController } from './common.controller'
+import { CommonService } from './common.service'
 
 describe('CommonController', () => {
-  let controller: CommonController;
+  let controller: CommonController
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CommonController],
       providers: [CommonService],
-    }).compile();
+    }).compile()
 
-    controller = module.get<CommonController>(CommonController);
-  });
+    controller = module.get<CommonController>(CommonController)
+  })
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
+    expect(controller).toBeDefined()
+  })
+})
--- packages/server/src/common/common.controller.ts
+++ packages/server/src/common/common.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UploadedFile, UseInterceptors, Req, Inject } from '@nestjs/common'
+import { Controller, Post, UploadedFile, UseInterceptors, Req, Inject, UseGuards, HttpException, HttpStatus, ParseFilePipe, MaxFileSizeValidator } from '@nestjs/common'
 import { CommonService } from './common.service'
 import R from 'src/utils/response'
 import { FileInterceptor } from '@nestjs/platform-express'
@@ -6,35 +6,11 @@ import { diskStorage } from 'multer'
 import { extname, resolve } from 'path'
 import { v4 as uuid_v4 } from 'uuid'
 import { Request } from 'express'
-import os from 'os'
 import { ApiTags } from '@nestjs/swagger'
-
-function getNetworkIp() {
-  let needHost = '' // 打开的host
-  try {
-    // 获得网络接口列表
-    const network = os.networkInterfaces()
-    // console.log("network",network)
-    for (const dev in network) {
-      const iface = network[dev]
-      for (let i = 0; i < iface.length; i++) {
-        const alias = iface[i]
-        if (
-          alias.family === 'IPv4' &&
-          alias.address !== '127.0.0.1' &&
-          !alias.internal
-        ) {
-          needHost = alias.address
-        }
-      }
-    }
-  } catch (e) {
-    needHost = 'localhost'
-  }
-  return needHost
-}
+import { AdminAuthGuard } from 'src/auth/AdminAuth.guard'
 
 @ApiTags('通用接口')
+@UseGuards(AdminAuthGuard)
 @Controller('/admin/common')
 export class CommonController {
   @Inject(CommonService)
@@ -43,28 +19,42 @@ export class CommonController {
   private host: string
 
   constructor() {
-    this.host = getNetworkIp()
+    this.host = process.env.MYSQL_HOST
   }
 
   @Post('/upload')
   @UseInterceptors(FileInterceptor('file', {
     storage: diskStorage({
       destination: (_, file, cb) => {
-        let temp = ''
-        if (file.mimetype.startsWith('image')) {
-          temp = '/images'
-        }
-        const path = resolve(process.cwd(), `./public${temp}`)
+        const path = resolve(process.cwd(), './public/images')
         cb(null, path)
       },
       filename: (_, file, cb) => {
         const ext = extname(file.originalname)
         const _filename = uuid_v4()
         cb(null, _filename + ext)
+      },
+    }),
+    fileFilter: (req: Request, file, cb) => {
+      if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        return cb(new HttpException('仅能上传 PNG JPEG JPG类型图片', HttpStatus.FORBIDDEN), false)
       }
-    })
+      cb(null, true)
+    }
   }))
-  uploadFile(@UploadedFile() file: Express.Multer.File, @Req() req: Request) {
+  uploadFile(
+    @UploadedFile(new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({
+          maxSize: 1024 * 1024 * 2,
+          message: '图片大小不超过2M',
+        }),
+      ]
+    }))
+    file: Express.Multer.File,
+    @Req()
+    req: Request
+  ) {
     const host = this.host
     const protocol = req.protocol
     return R.success(`${protocol}://${host}:${process.env.PORT}/public/images/${file.filename}`)
